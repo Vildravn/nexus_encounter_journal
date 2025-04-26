a2e7b8188900792e40a107b4a485190e72185efd
Vildravn <vildravn@gmail.com>
2025-04-26 21:06:12 +0000

    Add better centering of the SelectableCard text

--- src/imgui_custom.cpp
+++ src/imgui_custom.cpp
@@ -39,21 +39,29 @@ void CustomSelectable(std::function<void()> on_click, std::string label, bool se
 	}
 }
 
-void CustomSelectableCard(std::function<void()> on_click, std::string label, bool disabled)
+void CustomSelectableCard(std::function<void()> on_click, const std::string label, bool disabled)
 {
 	float width = ImGui::GetColumnWidth(ImGui::GetColumnIndex());
-	if (ImGui::Selectable(("##" + label).c_str(), false, disabled ? ImGuiSelectableFlags_Disabled : ImGuiSelectableFlags_None, ImVec2(width, 80)))
+	float height = 80.0f;
+	if (ImGui::Selectable(("##" + label).c_str(), false, disabled ? ImGuiSelectableFlags_Disabled : ImGuiSelectableFlags_None, ImVec2(width, height)))
 	{
 		on_click();
 	}
-	ImGui::SameLine();
-	ImVec2 pos = ImGui::GetCursorPos();
-	ImVec2 text_size = ImGui::CalcTextSize(label.c_str());
-	float center_x = pos.x - width / 2 - (text_size.x / 2);
-	float center_y = pos.y + 40 - (text_size.y / 2);
-	ImGui::SetCursorPos(ImVec2(center_x, center_y));
+
+	ImVec2 itemRectMin = ImGui::GetItemRectMin();
+	ImVec2 itemRectMax = ImGui::GetItemRectMax();
+	ImVec2 itemSize = ImGui::GetItemRectSize();
+	ImVec2 textSize = ImGui::CalcTextSize(label.c_str(), NULL, true, itemSize.x);
+	float textPosX = itemRectMin.x + (itemSize.x - textSize.x) * 0.5f;
+	float textPosY = itemRectMin.y + (itemSize.y - textSize.y) * 0.5f;
+	ImGui::SetCursorScreenPos(ImVec2(textPosX, textPosY));
+	ImGui::PushTextWrapPos(textPosX + itemSize.x);
+
 	if (disabled) ImGui::TextDisabled("%s", label.c_str());
 	else ImGui::TextWrapped("%s", label.c_str());
+
+	ImGui::PopTextWrapPos();
+	ImGui::SetCursorScreenPos(ImVec2(itemRectMin.x, itemRectMax.y));
 }
 
 void CustomSelectableBack(std::function<void()> on_click, std::string label)
